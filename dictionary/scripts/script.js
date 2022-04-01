3fc1d97a39e417216f4b8f0f2eb56780f630ae20
CrisGS <93886227+CrisGS@users.noreply.github.com>
2022-04-01 11:53:51 +0300

    Delete dictionary directory

--- dictionary/scripts/script.js
+++
@@ -1,7 +0,0 @@
-function addWords() {
-  let myDiv = document.createElement("div");
-  myDiv.id = "myWords";
-  let word = document.getElementById("myWord").value;
-  myDiv.innerHTML = "<li>"+ word +"</li>";
-  document.body.appendChild(myDiv);
-}
